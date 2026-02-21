089a7d6aea8522199146078eba300bc4b9bd5f36
Connor Fundell <cfundell@iastate.edu>
2026-02-20 19:44:21 -0600

    Added plotting capability and example plots

--- GOE.py
+++ GOE.py
@@ -19,7 +19,9 @@ def generate_GOE(N):
     return H
 
 # Assumes symmetric matrix
-def calculate_mean_and_var(H, N):
+# H is a GOE of size N
+# If print_results = True, prints the mean and variance of the diagonal and off-diagonal elements to the terminal
+def calculate_mean_and_var(H, N, print_results):
     diag_total = 0.0
     off_diag_total = 0.0
 
@@ -40,9 +42,17 @@ def calculate_mean_and_var(H, N):
     off_diag_mean = off_diag_total/(N*N - N)
     off_diag_variance = np.var(off_diag_entries)
 
+    if print_results:
+        print(f"Mean of diagonal elements: {diag_mean}")
+        print(f"Variance of diagonal elements: {diag_variance}")
+        print(f"Mean of off-diagonal elements: {off_diag_mean}")
+        print(f"Variance of off-diagonal elements: {off_diag_variance}")
+
     return diag_mean, diag_variance, off_diag_mean, off_diag_variance
 
-def calculate_eigval_spacing(H, N):
+# print_results = true: Prints average spacing and its deviation to terminal
+# plot_results = true: Plots histograms of the eigenvalues and eigenvalue spacings. Also plots a selected range of eigenvalues to visualize their spacings
+def calculate_eigval_spacing(H, N, print_results, plot_results):
     eigenvalues = np.linalg.eigvals(H)
     eigenvalues = -np.sort(-eigenvalues)
 
@@ -55,24 +65,73 @@ def calculate_eigval_spacing(H, N):
         total_spacing += spacing
 
     average_spacing = total_spacing/N
+    spacing_std = np.sqrt(np.var(spacings))
 
-    return average_spacing, spacings
+    if print_results:
+        print(f"\nAverage eigenvalue spacing: {average_spacing}")
+        print(f"Standard deviation of eigenvalue spacing: {spacing_std}")
+
+    if plot_results:
+        # Create the histogram probability density for eigenvalues
+        plt.hist(
+            eigenvalues,
+            bins=50,
+            range=(-eigenvalues[0], eigenvalues[0]),
+            density=True,        # y-axis becomes probability density
+            edgecolor="black",   # draw lines around bins
+            linewidth=0.5,
+            label="Eigenvalue histogram",
+            color="#ff9100")
+        plt.xlabel("lambda")
+        plt.ylabel("Probability density of lambda")
+        plt.title(f"Eigenvalue histogram for GOE with N = {N}")
+        plt.savefig("eigval_hist.png", dpi=300, bbox_inches='tight')
+        plt.close()
+
+        # Create histogram probability density for eigenvalue spacings
+        plt.hist(
+            spacings,
+            bins=25,
+            range=(0, 0.3),
+            density=True,        # y-axis becomes probability density
+            edgecolor="black",   # draw lines around bins
+            linewidth=0.5,
+            label="Eigenvalue histogram",
+            color="#ff9100")
+        plt.xlabel("|lambda_k - lambda_k+1|")
+        plt.ylabel("Probability density")
+        plt.title(f"Eigenvalue spacing histogram for GOE with N = {N}")
+        plt.savefig("spacings_hist.png", dpi=300, bbox_inches='tight')
+        plt.close()
+
+        # Plot eigenvalues to show spacings
+        x_range = []
+        for i in range(25):
+            x_range.append(eigenvalues[500 + i])
+        
+        y_range = []
+        for x in x_range:
+            y_range.append(1)
+
+        # Hide tick and label marks of y axis
+        fig, ax = plt.subplots()
+        ax.set_yticks([]) 
+        ax.set_yticklabels([]) 
+
+        plt.plot(x_range, y_range, "bo", markersize=1)
+        plt.xlabel("lambda")
+        plt.title(f"Eigenvalue spacing histogram for GOE with N = {N} in the range {eigenvalues[500]:.2f},{eigenvalues[524]:.2f}")
+        plt.savefig("eigvals.png", dpi=300, bbox_inches='tight')
+        plt.close()
+        
+    return average_spacing, spacings, eigenvalues
 
 
 # Initialize numpy random number generator
 rng = np.random.default_rng()
 
 # Test
-N = 100
+N = 1000
 H = generate_GOE(N)
-average_eigval_spacing, spacings = calculate_eigval_spacing(H,N)
-# print(f"Eigenvalue spacings:\n{spacings}")
-print(f"\nAverage eigenvalue spacing: {average_eigval_spacing}")
 
-
-diag_mean, diag_variance, off_diag_mean, off_diag_variance = calculate_mean_and_var(H, N)
-
-print(f"Mean of diagonal elements: {diag_mean}")
-print(f"Variance of diagonal elements: {diag_variance}")
-print(f"Mean of off-diagonal elements: {off_diag_mean}")
-print(f"Variance of off-diagonal elements: {off_diag_variance}")
+calculate_eigval_spacing(H, N, False, True)
